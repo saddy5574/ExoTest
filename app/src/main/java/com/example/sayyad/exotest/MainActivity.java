06c85d8ab856541c1892cf9c97778a87494047a6
Sayyad <saddy5774@gmail.com>
2018-11-02 21:30:04 -0700

    Change in Main Activity

--- app/src/main/java/com/example/sayyad/exotest/MainActivity.java
+++ app/src/main/java/com/example/sayyad/exotest/MainActivity.java
@@ -9,6 +9,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
 }
